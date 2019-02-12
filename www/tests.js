391975bffe63ae6d2c39f36f9f3a2552d8f399b2
kianenigma <Kian.peymani@gmail.com>
2019-02-12 23:29:37 +0100

    finalize js tests

--- www/tests.js
+++ www/tests.js
@@ -6,6 +6,14 @@ function generate_seed(count) {
     return seed
 } 
 
+String.prototype.getBytes = function () {
+    var bytes = [];
+    for (var i = 0; i < this.length; ++i) {
+        bytes.push(this.charCodeAt(i));
+    }
+    return bytes;
+};
+
 describe('Basic Signing and Verification', function () {
     before(function (done) {
         setTimeout(function() {
@@ -21,18 +29,44 @@ describe('Basic Signing and Verification', function () {
     });
 
     it('should create a standalone secret key', function () {
-
+        let SCHNORRKEL = window.schnorrkel
+        let seed = generate_seed(32)
+        let pair = SCHNORRKEL.secret_from_seed(seed)
+        chai.expect(pair.length).to.equal(64)
     })
 
     it('should sign a message', function () {
-
+        let SCHNORRKEL = window.schnorrkel
+        let message = "This is a message".getBytes()
+        let seed = generate_seed(32)
+        let keypair = SCHNORRKEL.keypair_from_seed(seed)
+        let private = keypair.slice(0, 64)
+        let public = keypair.slice(64, 96)
+        let signature = SCHNORRKEL.sign(public, private, message)
+        chai.expect(signature.length).to.equal(64)
     })
 
     it('should verify a signed message', function () {
-
+        let SCHNORRKEL = window.schnorrkel
+        let message = "This is a message".getBytes()
+        let seed = generate_seed(32)
+        let keypair = SCHNORRKEL.keypair_from_seed(seed)
+        let private = keypair.slice(0, 64)
+        let public = keypair.slice(64, 96)
+        let signature = SCHNORRKEL.sign(public, private, message)
+        chai.expect(SCHNORRKEL.verify(signature, message, public)).to.equal(true)
     })
 
     it('should reject a wrong signature', function() {
-
+        let SCHNORRKEL = window.schnorrkel
+        let message = "This is a message".getBytes()
+        let seed = generate_seed(32)
+        let keypair = SCHNORRKEL.keypair_from_seed(seed)
+        let private = keypair.slice(0, 64)
+        let public = keypair.slice(64, 96)
+        let signature = SCHNORRKEL.sign(public, private, message)
+        // mutate the signature
+        signature[0] = (signature[0] + 1) % 255
+        chai.expect(SCHNORRKEL.verify(signature, message, public)).to.equal(false)
     })
 });
